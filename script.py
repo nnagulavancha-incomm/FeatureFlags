6e38c85cb4263b31001c0559f491d4f460aba6b1
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2018-06-25 15:41:10 -0400

    python scriopt

--- script.py
+++ script.py
@@ -1,8 +1,21 @@
-import os, plistlib
+import os, plistlib, json, requests
+
+webhook_url = 'https://hooks.slack.com/services/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'
 
 dir_path = os.getcwd()
 plist_path = dir_path+'/FeatureFlags/Features.plist'
 
 pl = plistlib.readPlist(plist_path)
 
-print(pl)
+slack_data = pl
+
+response = requests.post(
+    webhook_url, json={"text":"Features\n"+json.dumps(slack_data)},
+    headers={'Content-Type': 'application/json'}
+)
+
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
